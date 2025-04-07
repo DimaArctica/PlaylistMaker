bb2a205f58103b946775ced49fab5897e4d87071
DimaArctica <saharniyutkonos@gmail.com>
2025-04-07 13:24:05 +0300

    Settings menu share button: first steps

--- app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.practium.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +13,22 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<FrameLayout>(R.id.shareButton)
+
+        shareButton.setOnClickListener {
+//            val message = "https://practicum.yandex.ru/profile/android-developer-plus/"
+//            val shareIntent = Intent(Intent.ACTION_CHOOSER)
+//            //shareIntent.action = Intent.ACTION_SEND
+//            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+//            startActivity(intent)
+            val message = "Привет, Android-разработка — это круто!"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("")
+            //shareIntent.putExtra(Intent.EXTRA_, arrayOf("yourEmail@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
     }
 }
