ef47655f3a05226b1783ca7baf9e58a692208784
DimaArctica <saharniyutkonos@gmail.com>
2025-04-08 11:54:24 +0300

    Media library and search activity stub page Settings activity - fix view

--- app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.practium.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,11 +8,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SettingsActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +15,12 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings_activity)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val shareButton = findViewById<FrameLayout>(R.id.shareButton)
         val supportButton = findViewById<FrameLayout>(R.id.supportButton)
