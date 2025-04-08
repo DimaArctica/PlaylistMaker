cdda1d43e7d454d5a558ca1dd3999be1c38be0c5
DimaArctica <saharniyutkonos@gmail.com>
2025-04-08 20:23:19 +0300

    Search activity EditText field - done

--- app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practium/playlistmaker/SearchActivity.kt
@@ -3,6 +3,10 @@ package com.practium.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,10 +26,37 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val goBackArrow = findViewById<ImageView>(R.id.arrowBackButton)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         goBackArrow.setOnClickListener {
             finish()
         }
 
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
