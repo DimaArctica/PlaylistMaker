b88205618802afe3bfc9aacd54c4aeda81185b33
DimaArctica <saharniyutkonos@gmail.com>
2025-04-23 22:21:19 +0300

    Search tracklist view - make styles and themes for elements

--- app/src/main/java/com/practium/playlistmaker/TrackListSearchViewHolder.kt
+++ app/src/main/java/com/practium/playlistmaker/TrackListSearchViewHolder.kt
@@ -6,21 +6,23 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackListSearchViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     private val trackName: TextView = itemView.findViewById(R.id.trackName)
     private val artistName: TextView = itemView.findViewById(R.id.artistName)
-    private val artwork: ImageView = itemView.findViewById<ImageView>(R.id.albumCover)
+    private val albumCover: ImageView = itemView.findViewById(R.id.albumCover)
 
-    @SuppressLint("SetTextI18n")
-    fun bind(model: Track) {
-        trackName.text = model.trackName
-        artistName.text = "${model.artistName} ● ${model.trackTime}"
+    @SuppressLint("SetTextI18n", "CheckResult")
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = "${track.artistName} • ${track.trackTime}"
         Glide.with(itemView)
-            .load(model.artworkUrl100)
+            .load(track.artworkUrl100)
+            .transform(RoundedCorners(2))
             .placeholder(R.drawable.album_cover_placeholder)
-            .into(artwork)
+            .into(albumCover)
     }
 
 }
