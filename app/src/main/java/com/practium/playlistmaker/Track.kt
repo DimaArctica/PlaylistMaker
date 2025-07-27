454c87bc8700a5035993adcbb1a24586d60de0e3
DimaArctica <saharniyutkonos@gmail.com>
2025-07-27 14:51:08 +0300

    Сверстана Activity аудиоплеера, базово работает

--- app/src/main/java/com/practium/playlistmaker/Track.kt
+++ app/src/main/java/com/practium/playlistmaker/Track.kt
@@ -1,8 +1,18 @@
 package com.practium.playlistmaker
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.Date
+
+@Parcelize
 data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Int,
     val artworkUrl100: String,
-    val trackId: Int)
+    val trackId: Int,
+    val country: String,
+    val primaryGenreName: String,
+    val collectionName: String,
+    val releaseDate: Date
+) : Parcelable
